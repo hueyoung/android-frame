8f3a8845729f52c2e85936327fccdfceed90d830
XC\000001523 <hueyoung@foxmail.com>
2017-08-23 16:37:58 +0800

    添加远程组件测试

--- firstbundle/src/main/java/com/ytl/frame/firstbundle/FirstActivity.java
+++ firstbundle/src/main/java/com/ytl/frame/firstbundle/FirstActivity.java
@@ -1,7 +1,9 @@
 package com.ytl.frame.firstbundle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ytl.frame.firstbundle.R;
 
@@ -11,5 +13,14 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+
+    // 加载远程bundle
+    public void loadRemoteBundle(View v) {
+        Intent intent = new Intent();
+        intent.setClassName(this, "com.ytl.frame.remotebundle.MainActivity");
+        startActivity(intent);
+        finish();
+        overridePendingTransition(-1,android.R.anim.slide_out_right);
     }
 }
