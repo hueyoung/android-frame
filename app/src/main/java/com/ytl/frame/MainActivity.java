2ea11d4cbc61743c0670c7e1a50574c16bf15b8a
XC\000001523 <hueyoung@foxmail.com>
2017-08-28 13:56:03 +0800

    添加远程组件模块&热部署更新

--- app/src/main/java/com/ytl/frame/MainActivity.java
+++ app/src/main/java/com/ytl/frame/MainActivity.java
@@ -1,9 +1,13 @@
 package com.ytl.frame;
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import com.ytl.frame.update.Updater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AsyncTask<Void, Void, Void>() {
+                    @Override
+                    protected Void doInBackground(Void... voids) {
+                        Updater.update(getBaseContext());
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void aVoid) {
+                        android.os.Process.killProcess(android.os.Process.myPid());
+                    }
+                }.execute();
+            }
+        });
     }
 
     public void goFirstAct(View v) {
--- firstbundle/src/main/java/com/ytl/frame/firstbundle/FirstActivity.java
+++ firstbundle/src/main/java/com/ytl/frame/firstbundle/FirstActivity.java
@@ -1,11 +1,10 @@
 package com.ytl.frame.firstbundle;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.ytl.frame.firstbundle.R;
 
 public class FirstActivity extends AppCompatActivity {
 
